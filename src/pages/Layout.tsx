26b1294abf080a6c35c8f4796bad00ec74ac1e3c
maxymlyskov <raypember02@gmail.com>
2023-09-02 21:29:12 +0300

    Add Layout.

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+
+const Layout = () => {
+  return (
+    <>
+      <Box padding={5}>
+        <Outlet />
+      </Box>
+    </>
+  );
+};
+
+export default Layout;
