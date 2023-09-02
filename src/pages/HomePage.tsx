210ff2bcef18f89e1e7a0c2d9318e67f2a5de8aa
maxymlyskov <raypember02@gmail.com>
2023-09-02 21:29:32 +0300

    Add Error page and home page layout.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Input } from "@chakra-ui/react";
+import React from "react";
+import Form from "../components/Form";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default HomePage;
