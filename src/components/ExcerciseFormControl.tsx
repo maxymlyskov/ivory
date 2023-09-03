9f90a12959e7562a0f63936e8328852644d8f737
maxymlyskov <raypember02@gmail.com>
2023-09-03 14:04:07 +0300

    Implement ExcerciseFormControl.

---
+++ src/components/ExcerciseFormControl.tsx
@@ -0,0 +1,38 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { FormData } from "./Form";
+import { FC } from "react";
+import { ExcerciseFormProps } from "../entitites/excercises";
+import { UseFormRegister } from "react-hook-form";
+
+interface Props {
+  excercise: ExcerciseFormProps;
+  isError: boolean;
+  register: UseFormRegister<FormData>;
+  errorMessage?: string;
+}
+
+const ExcerciseFormControl: FC<Props> = ({
+  excercise,
+  isError,
+  register,
+  errorMessage,
+}) => {
+  return (
+    <FormControl key={excercise.id} isInvalid={isError} width="400px">
+      <FormLabel htmlFor={excercise.id}>{excercise.label}</FormLabel>
+      <Input
+        id={excercise.id}
+        placeholder={excercise.placeholder}
+        {...register(excercise.id)}
+      />
+      <FormErrorMessage>{errorMessage || "Error occured!"}</FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default ExcerciseFormControl;
