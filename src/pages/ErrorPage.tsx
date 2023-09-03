ba73b83db5119d80ae2824688fccdfb26e46e9a1
maxymlyskov <raypember02@gmail.com>
2023-09-03 14:05:45 +0300

    Added UI to Error page.

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Box, Heading, Text, VStack, Button } from "@chakra-ui/react";
 
-const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+interface ErrorPageProps {
+  onRetry?: () => void;
+}
+
+const ErrorPage: React.FC<ErrorPageProps> = ({ onRetry }) => {
+  return (
+    <VStack
+      spacing={5}
+      alignItems="center"
+      height="100vh"
+      justifyContent="center"
+    >
+      <Box borderRadius="lg" padding={4} bg="red.100" boxShadow="md">
+        <Heading size="md">No Exercises Found</Heading>
+        <Text>It seems we couldn't find any exercises for you.</Text>
+      </Box>
+      {onRetry && (
+        <Button colorScheme="teal" onClick={onRetry}>
+          Retry
+        </Button>
+      )}
+    </VStack>
+  );
 };
 
 export default ErrorPage;
